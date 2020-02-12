c3c2188d97bed7799042e2515b3f538095b1e6d8
spenny180 <59981850+spenny180@users.noreply.github.com>
2020-02-11 20:49:58 -0600

    Update Program.cs

--- SummarizeTempsLab/SummarizeTempsLab/Program.cs
+++ SummarizeTempsLab/SummarizeTempsLab/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.IO;
 
 namespace SummarizeTempsLab
@@ -10,71 +11,76 @@ namespace SummarizeTempsLab
             string myFile = "data.txt";
             Console.WriteLine("Enter Data File Name");
             myFile = Console.ReadLine();
-
+            string ayyy = "";
             if (File.Exists(myFile))
             {
-                Console.WriteLine("Cool Beans");
-                int BaseTemp;
-                string input;
-                Console.WriteLine("Enter the threshold Temp");
-                input = Console.ReadLine();
-                BaseTemp = int.Parse(input);
-                
-                int Temperature;
-                int Above = 0;
-                int Below = 0;
-                int sum = 0;
-                int aboveSum = 0;
-                int belowSum = 0;
-                int avgAbove;
-                int avgBelow;
-                int mean;
-
-
-                using (StreamReader sr = File.OpenText(myFile)) 
+                do
                 {
-                    string line = sr.ReadLine();
-                    
+                    Console.WriteLine("Cool Beans");
+                    int BaseTemp;
+                    string input;
+                    Console.WriteLine("Enter the threshold Temp");
+                    input = Console.ReadLine();
+                    BaseTemp = int.Parse(input);
 
-                    while (line != null)
+                    int Temperature;
+                    int Above = 0;
+                    int Below = 0;
+                    int sum = 0;
+                    int aboveSum = 0;
+                    int belowSum = 0;
+                    int avgAbove;
+                    int avgBelow;
+                    int mean;
+
+
+                    using (StreamReader sr = File.OpenText(myFile))
                     {
-                        Temperature = int.Parse(line);
+                        string line = sr.ReadLine();
 
-                        if (Temperature >= BaseTemp)
-                        {
-                            Above++;
-                            sum = sum + Temperature;
-                            aboveSum = aboveSum + Temperature;
-                           
-                        }
-                        else
-                        {
-                            Below++;
-                            sum = sum + Temperature;
-                            belowSum = belowSum + Temperature;
 
+                        while (line != null)
+                        {
+                            Temperature = int.Parse(line);
+
+                            if (Temperature >= BaseTemp)
+                            {
+                                Above++;
+                                sum = sum + Temperature;
+                                aboveSum = aboveSum + Temperature;
+
+                            }
+                            else
+                            {
+                                Below++;
+                                sum = sum + Temperature;
+                                belowSum = belowSum + Temperature;
+
+                            }
+                            line = sr.ReadLine();
                         }
-                        line = sr.ReadLine();
+
                     }
 
-                }
+                    avgAbove = aboveSum / Above;
+                    avgBelow = belowSum / Below;
+                    mean = sum / (Above + Below);
 
-                avgAbove = aboveSum / Above;
-                avgBelow = belowSum / Below;
-                mean = sum / (Above + Below);
-
-                Console.WriteLine("The Number of Temps Above the Threshold is " + Above);
-                Console.WriteLine("The Average Temp Above the Threshold is " + avgAbove);
-                Console.WriteLine("The Number of Temps Below the Threshold is " + Below);
-                Console.WriteLine("The Average Temp Below the Threshold is " + avgBelow);
-                Console.WriteLine("The Overall Average Temp is " + mean);
-
+                    Console.WriteLine("The Number of Temps Above the Threshold is " + Above);
+                    Console.WriteLine("The Average Temp Above the Threshold is " + avgAbove);
+                    Console.WriteLine("The Number of Temps Below the Threshold is " + Below);
+                    Console.WriteLine("The Average Temp Below the Threshold is " + avgBelow);
+                    Console.WriteLine("The Overall Average Temp is " + mean);
+                    Console.WriteLine("Would you like to continue? Type yes or no");
+                    ayyy = Console.ReadLine();
+                } while (ayyy == "yes");
+                Console.WriteLine("Program has ended, Thank You {:");
             }
             else
             {
                 Console.WriteLine("File Does Not Exist");
             }
-            
+
         }
     }
 }
